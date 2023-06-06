0e5f912b44bf511236a2aff3175014348be41e82
Michele <m@pelosi.pictures>
2023-06-06 21:07:54 +0200

    D1

---
+++ esercizi.js
@@ -0,0 +1,23 @@
+let sugarTop = 80
+let sugarTop = null
+
+let burro =120
+let zucchero = 80
+let farina = 260
+
+let ciotolaMedia = burro + zucchero
+let ciotolaMedia = ciotolaMedia + farina
+
+ciotolaMedia += farina
+// x += y is equivalent to x = x + y
+
+/* Un algoritmo è un meccanismo che tramite delle istruzioni ben precise
+trasforma i dati inseriti input in una soluzione finale ad un determinato problema
+Come in una ricetta, una serie di ingredienti forniti in input vengono elaborati e trattati 
+con una ricetta specifica per trasformarli in un piatto finale */
+
+// Una variabile è un dato specifico fornito ed etichettato, uno degli ingredienti della ricetta
+// null svuota il campo, rende vuota la variabile è un parametro definito vuoto
+// undefined invece è un parametro che non è ancora stato dichiarato definito
+
+console.log(sugarTop);
